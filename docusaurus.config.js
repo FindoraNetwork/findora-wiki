5f1afa281d959e950eeefcd74794cb7caf4896c5
David Lin <dlin128@gmail.com>
2021-06-27 17:50:39 -0700

    change edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,13 +83,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/FindoraNetwork/findora-wiki/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
