9deabe1a43ebdb222584a375279e4c2e6a086e0a
Jimmy <jimmy@findora.org>
2021-07-26 15:17:57 -0700

    update the base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'Findora Wiki',
   tagline: 'Dinosaurs are cool',
   url: 'https://wiki.findora.org/',
-  baseUrl: '/',
+  baseUrl: '/findora-wiki/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
