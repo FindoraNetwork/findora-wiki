e43fa8e28b22818584592a4fb4687ea7db27611b
simonjiao <simon@findora.org>
2021-08-09 18:34:18 +0800

    update node_init.sh for testnet

--- docs/general/node_init.sh
+++ docs/general/node_init.sh
@@ -14,6 +14,11 @@ check_env() {
             exit 1
         fi
     done
+
+    if ! [ -f "$keypath" ]; then
+        echo "The key file doesnot exist: $keypath"
+        exit 1
+    fi
 }
 
 set_binaries() {
@@ -21,17 +26,18 @@ set_binaries() {
 
     docker pull public.ecr.aws/k6m5b6e2/release/findorad:latest || exit 1
     wget -T 10 https://github.com/FindoraNetwork/testnet-downloads/releases/download/${OS}/fns || exit 1
-    wget -T 10 https://github.com/FindoraNetwork/testnet-downloads/releases/download/${OS}/stt || exit 1
 
-    new_path=/tmp/findora_testnet_bin
+    new_path=${ROOT_DIR}/bin
 
     rm -rf $new_path 2>/dev/null
     mkdir -p $new_path || exit 1
-    mv fns stt $new_path || exit 1
+    mv fns $new_path || exit 1
     chmod +x ${new_path}/* || exit 1
-
-    export PATH=${new_path}:${PATH}
 }
+
+export ROOT_DIR=${HOME}/findora_testnet
+keypath=${ROOT_DIR}/testnet_node.key
+FNS=${ROOT_DIR}/bin/fns
 
 check_env
 
@@ -47,47 +53,33 @@ else
 fi
 
 ######################
-# Config locale node #
+# Config local node #
 ######################
 
-export ROOT_DIR=${HOME}/findora_testnet
-
-keypath=/tmp/testnet_node.key
-fns genkey > $keypath || exit 1
+#fns genkey > $keypath || exit 1
 node_mnemonic=$(cat ${keypath} | grep 'Mnemonic' | sed 's/^.*Mnemonic:[^ ]* //')
 xfr_pubkey="$(cat ${keypath} | grep 'pub_key' | sed 's/[",]//g' | sed 's/ *pub_key: *//')"
 
-fns setup -S ${SERV_URL} || exit 1
-fns setup -K ~/.tendermint/config/priv_validator_key.json || exit 1
-
-rm -rf ${ROOT_DIR}
-mkdir -p ${ROOT_DIR}
 echo $node_mnemonic > ${ROOT_DIR}/node.mnemonic || exit 1
-fns setup -O ${ROOT_DIR}/node.mnemonic || exit 1
 
-stt issue || exit 1
-stt transfer -f root -t ${xfr_pubkey} -n $((10000 * 10000 * 1000000)) || exit 1
-sleep 30
+$FNS setup -S ${SERV_URL} || exit 1
+$FNS setup -K ${HOME}/.tendermint/config/priv_validator_key.json || exit 1
+$FNS setup -O ${ROOT_DIR}/node.mnemonic || exit 1
 
-if [[ 0 -eq `fns show 2>&1 | grep -A 1 "Node Balance" | sed 's/ FRA units *$//' | tail -1` ]]; then
-    echo -e "Transfer FRAs to your address failed !"
-    exit 1
-fi
+# clean old data and config files
+sudo rm -rf ${ROOT_DIR}/findorad || exit 1
+mkdir -p ${ROOT_DIR}/findorad || exit 1
 
-docker run --rm -v $HOME/.tendermint:/root/.tendermint public.ecr.aws/k6m5b6e2/release/findorad init --test-net || exit 1
+docker run --rm -v ${HOME}/.tendermint:/root/.tendermint public.ecr.aws/k6m5b6e2/release/findorad init --test-net || exit 1
 
 ###################
-# Run locale node #
+# Run local node #
 ###################
 
-docker stop findorad
-docker rm findorad
-mkdir -p ${ROOT_DIR}/{abci,tendermint}
-
-cd ${ROOT_DIR}/abci
+docker rm -f findorad || exit 1
 docker run -d \
     -v $HOME/.tendermint:/root/.tendermint \
-    -v $ROOT_DIR/abci:/tmp/findora \
+    -v $ROOT_DIR/findorad:/tmp/findora \
     -p 8669:8669 \
     -p 8668:8668 \
     -p 8667:8667 \
@@ -99,20 +91,16 @@ docker run -d \
     --enable-ledger-service \
     --enable-query-service
 
-sleep 5
+sleep 10
 
 curl 'http://localhost:26657/status'; echo
 curl 'http://localhost:8669/version'; echo
 curl 'http://localhost:8668/version'; echo
 curl 'http://localhost:8667/version'; echo
 
-stt issue || exit 1
-stt transfer -f root -t ${xfr_pubkey} -n $((889988 * 1000000)) || exit 1
-sleep 30
-
 if [[ 0 -eq `fns show 2>&1 | grep -A 1 "Node Balance" | sed 's/ FRA units *$//' | tail -1` ]]; then
     echo -e "Transfer FRAs to your address failed !"
     exit 1
 fi
 
-fns stake -n 888888000000 -R 0.1
+$FNS stake -n 888888000000 -R 0.1
