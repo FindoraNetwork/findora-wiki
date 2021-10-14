e7250200c9206d340615b2e8e597d0fffcfd9961
simonjiao <simon@findora.org>
2021-10-14 17:06:29 +0800

    update safety_clean for testnet upgrade

--- docs/guides/safety_clean.sh
+++ docs/guides/safety_clean.sh
@@ -7,8 +7,9 @@ if [ ! $ROOT_DIR ]; then
     echo 'Please set ROOT_DIR first.'
 fi
 
+TM_HOME=${ROOT_DIR}/tendermint
 
-docker pull findoranetwork/findorad:testnet-v0.2.0Sa-without-evm-compatible  || exit 1
+docker pull findoranetwork/findorad:v0.2.0-beta-4  || exit 1
 
 docker rm -f findorad || exit 1
 
@@ -16,11 +17,11 @@ sudo rm -rf $ROOT_DIR/findorad/*
 
 sudo rm -rf $ROOT_DIR/findorad/._*
 
-sudo rm -rf $HOME/.tendermint/data/*
+sudo rm -rf "${TM_HOME}"/data/*
 
-sudo chown -R `id -u`:`id -g` ${HOME}/.tendermint/data
+sudo chown -R `id -u`:`id -g` "${TM_HOME}"/data
 
-cat > ~/.tendermint/data/priv_validator_state.json << EOF
+cat > "{TM_HOME}"/data/priv_validator_state.json << EOF
 {
   "height": "0",
   "round": "0",
@@ -28,15 +29,15 @@ cat > ~/.tendermint/data/priv_validator_state.json << EOF
 }
 EOF
 
-sed -i "s#^timeout_propose = .*#timeout_propose = \"8s\"#g" "${HOME}/.tendermint/config/config.toml"
-sed -i "s#^timeout_propose_delta = .*#timeout_propose_delta = \"100ms\"#g" "${HOME}/.tendermint/config/config.toml"
-sed -i "s#^timeout_prevote = .*#timeout_prevote = \"4s\"#g" "${HOME}/.tendermint/config/config.toml"
-sed -i "s#^timeout_prevote_delta = .*#timeout_prevote_delta = \"100ms\"#g" "${HOME}/.tendermint/config/config.toml"
-sed -i "s#^timeout_precommit = .*#timeout_precommit = \"4s\"#g" "${HOME}/.tendermint/config/config.toml"
-sed -i "s#^timeout_precommit_delta = .*#timeout_precommit_delta = \"100ms\"#g" "${HOME}/.tendermint/config/config.toml"
-sed -i "s#^create_empty_blocks_interval = .*#create_empty_blocks_interval = \"15s\"#g" "${HOME}/.tendermint/config/config.toml"
-sed -i "s#^recheck = .*#recheck = \"false\"#g" "${HOME}/.tendermint/config/config.toml"
-sed -i "s#^fast_sync = .*#fast_sync = \"false\"#g" "${HOME}/.tendermint/config/config.toml"
+sed -i "s#^timeout_propose = .*#timeout_propose = \"8s\"#g" "${TM_HOME}/config/config.toml"
+sed -i "s#^timeout_propose_delta = .*#timeout_propose_delta = \"100ms\"#g" "${TM_HOME}/config/config.toml"
+sed -i "s#^timeout_prevote = .*#timeout_prevote = \"4s\"#g" "${TM_HOME}/config/config.toml"
+sed -i "s#^timeout_prevote_delta = .*#timeout_prevote_delta = \"100ms\"#g" "${TM_HOME}/config/config.toml"
+sed -i "s#^timeout_precommit = .*#timeout_precommit = \"4s\"#g" "${TM_HOME}/config/config.toml"
+sed -i "s#^timeout_precommit_delta = .*#timeout_precommit_delta = \"100ms\"#g" "${TM_HOME}/config/config.toml"
+sed -i "s#^create_empty_blocks_interval = .*#create_empty_blocks_interval = \"15s\"#g" "${TM_HOME}/config/config.toml"
+sed -i "s#^recheck = .*#recheck = \"false\"#g" "${TM_HOME}/config/config.toml"
+sed -i "s#^fast_sync = .*#fast_sync = \"false\"#g" "${TM_HOME}/config/config.toml"
 
 
 ###################
@@ -49,23 +50,23 @@ CHAINDATA_URL=$(cut -d , -f 1 "${ROOT_DIR}/latest")
 echo $CHAINDATA_URL
 # remove old data 
 rm -rf "${ROOT_DIR}/findorad"
-rm -rf "${HOME}/.tendermint/data"
-rm "${HOME}/.tendermint/config/addrbook.json"
+rm -rf "${TM_HOME}/data"
+rm "${TM_HOME}/config/addrbook.json"
 wget -O "${ROOT_DIR}/snapshot" "${CHAINDATA_URL}" 
 mkdir "${ROOT_DIR}/snapshot_data"
 tar zxvf "${ROOT_DIR}/snapshot" -C "${ROOT_DIR}/snapshot_data"
 cp -r "${ROOT_DIR}/snapshot_data/data/ledger" "${ROOT_DIR}/findorad"
-cp -r "${ROOT_DIR}/snapshot_data/data/tendermint/mainnet/node0/data" "${HOME}/.tendermint/data"
+cp -r "${ROOT_DIR}/snapshot_data/data/tendermint/mainnet/node0/data" "${TM_HOME}/data"
 
 docker run -d \
-    -v $HOME/.tendermint:/root/.tendermint \
+    -v $TM_HOME:/root/.tendermint \
     -v $ROOT_DIR/findorad:/tmp/findora \
     -p 8669:8669 \
     -p 8668:8668 \
     -p 8667:8667 \
     -p 26657:26657 \
     --name findorad \
-    findoranetwork/findorad:testnet-v0.2.0Sa-without-evm-compatible node \
+    findoranetwork/findorad:v0.2.0-beta-4 node \
     --ledger-dir /tmp/findora \
     --tendermint-host 0.0.0.0 \
     --tendermint-node-key-config-path="/root/.tendermint/config/priv_validator_key.json" \
