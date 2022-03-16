0ad2b4dc7ae883872614607e1ac99046287f9d41
jeffersonnnn <jefferie.halo@gmailcom>
2022-03-16 20:23:19 +0100

    adding files

--- docs/developers/evm/web3-integration.md
+++ docs/developers/evm/web3-integration.md
@@ -1,5 +1,5 @@
 ---
-sidebar_position: 2
+sidebar_position: 21
 ---
 
 # Web3 Integration
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,5 @@ import {Redirect} from '@docusaurus/router';
 
 
 export default function Home() {
-<<<<<<< HEAD
   return <Redirect to="/docs/findora-basics/intro" />;
-=======
-  return <Redirect to="/docs/general/intro" />;
->>>>>>> main
 }
