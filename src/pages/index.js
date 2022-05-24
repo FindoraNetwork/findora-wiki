531fd30abdc2c22056cd563bdb796d916604e275
David Lin <dlin128@gmail.com>
2022-05-24 02:33:49 -0700

    Add homepage
    Adding original homepage back. Will use as starting point to restyle.
    
    Minor rearranging

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
-import {Redirect} from '@docusaurus/router';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
 
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/introduction/intro">
+            Get started
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
 
 export default function Home() {
-  return <Redirect to="/docs/introduction/intro" />;
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Learn, Build and Run on Findora Network - ${siteConfig.title}`}
+      description="Learn, Build and Run on Findora Network - Findora Wiki">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
 }
