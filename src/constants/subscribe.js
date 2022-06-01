6f986fa32388aa996272050f8d73e656b2c7cbee
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2022-06-01 17:55:43 -0400

    feat: fo-993: finish mailchimp integration (using free demo account)

---
+++ src/constants/subscribe.js
@@ -0,0 +1,4 @@
+export const mailChimpUserId = process.env.REACT_APP_MAILCHIMP_U;
+export const mailChimpId = process.env.REACT_APP_MAILCHIMP_ID;
+
+export const mailChimpPostUrl = `https://gmail.us12.list-manage.com/subscribe/post?u=${mailChimpUserId}&id=${mailChimpId}`;
